5bf581902d756cafd854f87e6b3be0c08e01c525
Dilkhush Raj <dilkhush_raj@outlook.com>
2022-02-22 23:48:19 +0530

    PWA passed

--- sw.js
+++ sw.js
@@ -1,9 +1,52 @@
-// install service worker
+const staticCacheName = 'site-static-v1';
+const assets = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/script.js",
+  "/css/style.css",
+  "/css/clouds.css",
+  "/img/logo.svg",
+  "/img/hero-img.svg",
+  "/img/hero-img-dark.svg",
+  "/img/Dilkhush_Raj-150px.webp",
+  "https://unpkg.com/aos@next/dist/aos.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css",
+  "https://unpkg.com/aos@next/dist/aos.js",
+  "https://unpkg.com/typewriter-effect@latest/dist/core.js",
+];
+
+// install event
 self.addEventListener('install', evt => {
-    console.log('service worker has been installed')
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
-// active service worker
+// activate event
 self.addEventListener('activate', evt => {
-    console.log('service worker has been activated');
+  console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
